4de7a3f870d16bb2bd1717a5156a4ab7024ebee2
Fándly Gergő <gergo@paymoapp.com>
2024-11-20 13:02:04 +0200

    chore: make it work as a fork

--- examples/main.go
+++ examples/main.go
@@ -28,7 +28,7 @@ package main
 import (
 	_ "bytes"
 	"fmt"
-	"github.com/dutchcoders/go-clamd"
+	"github.com/gergof/go-clamd"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dutchcoders/go-clamd
+module github.com/gergof/go-clamd
 
 go 1.22.4
